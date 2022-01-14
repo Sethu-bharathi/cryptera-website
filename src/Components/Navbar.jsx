13ed646b09d525946e434d06c2aeb39a641bc7d0
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-01-14 14:58:09 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,18 @@
 
 import React, { useEffect, useRef } from 'react'
-import { div, Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import '../Styles/ComponentsStyles/Navbar.css'
 function Navbar() {
     const location = useLocation()
+    const navigate = useNavigate()
     const homeref = useRef(null)
     const aboutref = useRef(null)
     const contactref = useRef(null)
     const eventref = useRef(null)
     const webref = useRef(null)
     const commiteeref = useRef(null)
-    const clearActive=()=>{
+    const hintref = useRef(null)
+    const clearActive = () => {
         homeref.current.classList.remove('active')
         aboutref.current.classList.remove('active')
         contactref.current.classList.remove('active')
@@ -19,56 +21,77 @@ function Navbar() {
         commiteeref.current.classList.remove('active')
     }
     useEffect(() => {
-        if(location.pathname==="/"){
+        if (location.pathname === "/") {
             clearActive()
             homeref.current.classList.add("active")
+            hintref.current.innerHTML = "Scroll"
+            hintref.current.addEventListener("click", () => {
+
+            })
+
         }
-        else if(location.pathname==="/about_us"){
+        else if (location.pathname === "/about_us") {
             clearActive()
             aboutref.current.classList.add("active")
+            hintref.current.innerHTML = "Events"
+            hintref.current.addEventListener("click", () => {
+                navigate('/events')
+            })
         }
-        else if(location.pathname==="/contact_us"){
+        else if (location.pathname === "/contact_us") {
             clearActive()
             contactref.current.classList.add("active")
+            hintref.current.innerHTML = "To top"
+            hintref.current.addEventListener("click", () => {
+                navigate("/")
+            })
         }
-        else if(location.pathname==="/events"){ 
+        else if (location.pathname === "/events") {
             clearActive()
             eventref.current.classList.add("active")
+            hintref.current.innerHTML = "Web devs"
+            hintref.current.addEventListener("click", () => {
+                navigate("/web_devs")
+            })
         }
-        else if(location.pathname==="/web_devs"){
+        else if (location.pathname === "/web_devs") {
             clearActive()
             webref.current.classList.add("active")
+            hintref.current.innerHTML = "Committee"
+            hintref.current.addEventListener("click", () => {
+                navigate("/commitee")
+            })
+
         }
-        else if(location.pathname==="/commitee"){
+        else if (location.pathname === "/commitee") {
             clearActive()
             commiteeref.current.classList.add("active")
+            hintref.current.innerHTML = "Contact us"
+            hintref.current.addEventListener("click", () => {
+                navigate("/contact_us")
+            })
         }
-        else{
+        else {
             clearActive()
         }
     }, [location])
-    // const link_click = (e) => {
-    //     const Link_Nodes = document.querySelectorAll('.navbar-link')
-    //     Link_Nodes.forEach(node => {
-    //         node.classList.remove('active')
-    //     })
-    //     e.target.classList.add('active')
-    // }
+
     return (
         <div className='navbar'>
             <div className='navbar-logo'>
                 MD
             </div>
             <div className='navbar-links'>
-                <Link to="/" ref={homeref}  className="active navbar-link" id="Home-link">Home</Link>
-                <Link to="about_us" ref={aboutref}  id="about_us" className="navbar-link" id="about_us-link">About us</Link>
-                <Link to="events" ref={eventref}  className=" navbar-link">Events</Link>
-                <Link to="web_devs" ref={webref}  className="navbar-link">Web devs</Link>
-                <Link to="commitee" ref={commiteeref}  className=" navbar-link">Commitee</Link>
-                <Link to="contact_us" ref={contactref}  className=" navbar-link">Contact Us</Link>
+
+                <Link to="/" ref={homeref} className="active navbar-link" id="Home-link">       <i className="fa fa-home"></i><span>Home</span></Link>
+                <Link to="about_us" ref={aboutref} id="about_us" className="navbar-link" id="about_us-link"><i className="fa fa-info-circle"></i><span>About us</span></Link>
+                <Link to="events" ref={eventref} className=" navbar-link"> <i className="fa fa-calendar-minus"></i><span>Events</span></Link>
+                <Link to="web_devs" ref={webref} className="navbar-link"><i className="fa fa-wifi"></i><span>Web devs</span></Link>
+                <Link to="commitee" ref={commiteeref} className=" navbar-link"><i className="fa fa-users"></i><span>Commitee</span></Link>
+                <Link to="contact_us" ref={contactref} className=" navbar-link"><i className="fa fa-headset"></i><span>Contact Us</span></Link>
 
             </div>
-            <div className='bottom-special'>
+            <div className='bottom-special' ref={hintref}>
                 Scroll
             </div>
         </div>
