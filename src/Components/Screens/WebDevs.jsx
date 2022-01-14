d76bb95ee939e8719ea458f30cf7e9812b6a4c55
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-01-14 16:19:34 +0530

    web developers page

--- src/Components/Screens/WebDevs.jsx
+++ src/Components/Screens/WebDevs.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import '../../Styles/WebDevs.css'
+import profile1 from '../../Assets/1905098.png'
+import { useNavigate } from 'react-router-dom'
 function WebDevs() {
+    const webdevsref=useRef(null)
+    const navigate=useNavigate()
+    const scrollhandler=()=>{
+        if (webdevsref.current.offsetHeight+webdevsref.current.scrollTop>webdevsref.current.scrollHeight){
+            navigate("/commitee")
+        }
+    }
     return (
-        <div>
-           Web devs 
+        <div className='webdevs' ref={webdevsref} onScroll={scrollhandler}>
+            <div className='webdevs-container'>
+                <div >
+                    <h1 className='webdevs-title'>Web Developers</h1>
+                    <p className='webdevs-tilte-description'>Join the community around the world</p>
+                </div>
+                <div className='content-container'>
+                    <div className='web-card'>
+                        <div className='web-card-image'>
+                            <img src={profile1} alt='webdevs-1' />
+                        </div>
+                        <div className='web-card-content'>
+                            <div className='name'>Naveenkumar M</div>
+                            <div className='description'>I am a web developer from bombay is it yes thats true i am from bombay</div>
+                            <div className='media-container'>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-github"></i>
+                                <i class="fab fa-medium"></i>
+                                <i class="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='web-card'>
+                        <div className='web-card-image'>
+                            <img src={profile1} alt='webdevs-1' />
+                        </div>
+                        <div className='web-card-content'>
+                            <div className='name'>Naveenkumar M</div>
+                            <div className='description'>I am a web developer from bombay is it yes thats true i am from bombay</div>
+                            <div className='media-container'>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-github"></i>
+                                <i class="fab fa-medium"></i>
+                                <i class="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='web-card'>
+                        <div className='web-card-image'>
+                            <img src={profile1} alt='webdevs-1' />
+                        </div>
+                        <div className='web-card-content'>
+                            <div className='name'>Naveenkumar M</div>
+                            <div className='description'>I am a web developer from bombay is it yes thats true i am from bombay</div>
+                            <div className='media-container'>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-github"></i>
+                                <i class="fab fa-medium"></i>
+                                <i class="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='web-card'>
+                        <div className='web-card-image'>
+                            <img src={profile1} alt='webdevs-1' />
+                        </div>
+                        <div className='web-card-content'>
+                            <div className='name'>Naveenkumar M</div>
+                            <div className='description'>I am a web developer from bombay is it yes thats true i am from bombay</div>
+                            <div className='media-container'>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-github"></i>
+                                <i class="fab fa-medium"></i>
+                                <i class="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='web-card'>
+                        <div className='web-card-image'>
+                            <img src={profile1} alt='webdevs-1' />
+                        </div>
+                        <div className='web-card-content'>
+                            <div className='name'>Naveenkumar M</div>
+                            <div className='description'>I am a web developer from bombay is it yes thats true i am from bombay</div>
+                            <div className='media-container'>
+                                <i class="fab fa-instagram"></i>
+                                <i class="fab fa-github"></i>
+                                <i class="fab fa-medium"></i>
+                                <i class="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
     )
 }
